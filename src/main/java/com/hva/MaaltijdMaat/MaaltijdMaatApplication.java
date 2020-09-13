e7e37ce20419c254366154da208efed9a71791a6
Wouter Verdegaal <unconfigured@null.spigotmc.org>
2020-09-13 23:37:18 +0200

    first commit

---
+++ src/main/java/com/hva/MaaltijdMaat/MaaltijdMaatApplication.java
@@ -0,0 +1,13 @@
+package com.hva.MaaltijdMaat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaltijdMaatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaaltijdMaatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hva/MaaltijdMaat/MaaltijdMaatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hva.MaaltijdMaat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaaltijdMaatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
