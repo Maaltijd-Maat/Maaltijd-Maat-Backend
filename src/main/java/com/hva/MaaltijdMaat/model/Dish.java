ba5988cafbaf8355b53708ce75ef5d5e8fda17f0
René Kok <rkok98@gmail.com>
2020-09-30 17:50:02 +0200

    Updated Dish class

--- src/main/java/com/hva/MaaltijdMaat/model/Dish.java
+++ src/main/java/com/hva/MaaltijdMaat/model/Dish.java
@@ -1,16 +1,81 @@
 package com.hva.MaaltijdMaat.model;
 
 import lombok.AllArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.bson.types.ObjectId;
 
-import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @AllArgsConstructor
 public class Dish {
+
     @Id
-    private Long dishId;
+    private ObjectId id;
+
     private String name;
-    private List<String> steps;
+
+    @DBRef
+    private User user;
+
+    private int amountOfPeople;
+
+    private String[] instructions;
+
+    private Ingredient[] ingredients;
+
+    public Dish() {}
+
+    public Dish(String name, User user, int amountOfPeople, String[] instructions, Ingredient[] ingredients) {
+        this.name = name;
+        this.user = user;
+        this.amountOfPeople = amountOfPeople;
+        this.instructions = instructions;
+        this.ingredients = ingredients;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getAmountOfPeople() {
+        return amountOfPeople;
+    }
+
+    public void setAmountOfPeople(int amountOfPeople) {
+        this.amountOfPeople = amountOfPeople;
+    }
+
+    public String[] getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String[] instructions) {
+        this.instructions = instructions;
+    }
+
+    public Ingredient[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Ingredient[] ingredients) {
+        this.ingredients = ingredients;
+    }
 }
